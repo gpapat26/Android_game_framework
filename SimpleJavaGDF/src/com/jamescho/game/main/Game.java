e27b74973934fef9db2ced7a0841608b7649c4b3
gpapatheodorou <gpapatheodorou@globoplc.com>
2015-04-21 16:54:15 +0300

    initial commit

---
+++ SimpleJavaGDF/src/com/jamescho/game/main/Game.java
@@ -0,0 +1,48 @@
+package com.jamescho.game.main;
+
+import javax.swing.JPanel;
+
+import com.jamescho.game.state.LoadState;
+import com.jamescho.game.state.State;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java .awt.Image;
+
+@SuppressWarnings("serial")
+public class Game extends JPanel{
+	
+	private int gameWidth;
+	private int gameHeight;
+	private Image gameImage;
+	
+	private Thread gameThread;
+	
+	private volatile State currentState;
+	
+	private volatile boolean running;
+	
+	public Game(int gameWidth , int gameHeight){
+		this.gameHeight= gameHeight;
+		this.gameWidth = gameWidth;
+		setPreferredSize(new Dimension(gameWidth, gameHeight));
+		setBackground(Color.BLACK);
+		setFocusable(true);
+		requestFocus();
+		
+	}
+	
+	public void setCurrentState(State newState){
+		System.gc();
+		newState.init();
+		currentState = newState;
+	}
+	
+	@Override
+	public void addNotify(){
+		super.addNotify();
+		setCurrentState(new LoadState());
+	}
+	
+	
+}
---
+++ SimpleJavaGDF/src/com/jamescho/game/main/GameMain.java
@@ -0,0 +1,29 @@
+package com.jamescho.game.main;
+
+import javax.swing.JFrame;
+
+public class GameMain {
+	
+	private static final String  GAME_TITLE = "Java game dev chap 3";
+	public static final int GAME_WIDTH = 800;
+	public static final int GAME_HEIGHT = 450 ;
+	public static Game sGame;
+	
+	
+	public static void main (String ... args){
+		
+		JFrame frame = new JFrame(GAME_TITLE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false); // prevent manual resizing of window.
+		frame.setVisible(true);
+		
+		sGame = new Game(GAME_WIDTH,GAME_HEIGHT);
+		frame.add(sGame);
+		frame.pack();
+		frame.setVisible(true);
+		
+	}
+	
+	
+
+}
