ca599a2080f8fbb11c61e35bddefb71acb63e016
gpapatheodorou <gpapatheodorou@globoplc.com>
2015-04-28 17:59:16 +0300

    resources

--- Elio/src/com/jamescho/game/main/Resources.java
+++ Elio/src/com/jamescho/game/main/Resources.java
@@ -10,11 +10,27 @@ import javax.imageio.ImageIO;
 
 public class Resources {
 	
-	public static BufferedImage welcome , iconImage;
+	public static BufferedImage welcome , iconImage,block, cloud1,cloud2,duck,grass,jump,run1,run2,run3,run4,run5,selector;
+	public static AudioClip hit,onjump;
 	
 	public static void load(){
 		 welcome = loadImage("welcome.png");
-		 iconImage = loadImage("iconimage.png");
+		 iconImage = loadImage("iconimage.png");;
+		 block = loadImage("block.png");
+		 cloud1 = loadImage("cloud1.png");
+		 cloud2 = loadImage("cloud2.png");
+		 duck = loadImage("duck.png");
+		 grass = loadImage("grass.png");
+		 jump =loadImage("jump.png");
+		 run1 = loadImage("run_anim1.png");
+		 run2 = loadImage("run_anim2.png");
+		 run3 = loadImage("run_anim3.png");
+		 run4 = loadImage("run_anim4.png");
+		 run5 = loadImage("run_anim5.png");
+		 selector = loadImage("selector.png");
+		 hit = loadSound("hit.wav");
+		 onjump = loadSound("onjump.wav");
+		 
 	}
 	
 	
