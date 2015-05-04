6f256bd00c366ffe2a2d41229ad6e019fc061a06
gpapatheodorou <gpapatheodorou@globoplc.com>
2015-05-04 17:07:33 +0300

    MenuState

--- Elio/src/com/jamescho/game/state/MenuState.java
+++ Elio/src/com/jamescho/game/state/MenuState.java
@@ -4,9 +4,13 @@ import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
+import com.jamescho.game.main.GameMain;
 import com.jamescho.game.main.Resources;
+import com.sun.media.jfxmedia.events.PlayerStateEvent;
 
 public class MenuState extends State {
+	
+	private int currentSelection = 0;
 
 	@Override
 	public void init() {
@@ -23,6 +27,12 @@ public class MenuState extends State {
 	@Override
 	public void render(Graphics g) {
 		g.drawImage(Resources.welcome, 0, 0, null);
+		if(currentSelection == 0){
+			g.drawImage(Resources.selector, 335, 241, null);
+		}
+		else{
+			g.drawImage(Resources.selector, 335, 291, null);
+		}
 
 	}
 
@@ -36,7 +46,22 @@ public class MenuState extends State {
 	@Override
 	public void onKeyPress(KeyEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println(" key pressed !");
+		//System.out.println(" key pressed !");
+		int key = e.getKeyCode();
+		
+		if (key==KeyEvent.VK_SPACE || key == KeyEvent.VK_ENTER){
+			if (currentSelection ==0){
+				setCurrentState(new PlayState());
+			}
+			else if (currentSelection ==1){
+				GameMain.sGame.exit();
+			}
+		}
+		else if(key == KeyEvent.VK_UP){
+			currentSelection = 0;
+		}else if (key == KeyEvent.VK_DOWN){
+			currentSelection = 1;
+		}
 
 	}
 
---
+++ Elio/src/com/jamescho/game/state/PlayState.java
@@ -0,0 +1,45 @@
+package com.jamescho.game.state;
+
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+public class PlayState extends State {
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(float delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(Graphics g) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onClick(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onKeyPress(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onKeyRelease(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
