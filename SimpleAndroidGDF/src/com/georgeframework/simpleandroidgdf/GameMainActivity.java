589689cbe72da4f4bb8daaf70c81d5b90a9840c3
gpapatheodorou <gpapatheodorou@globoplc.com>
2015-05-11 18:01:51 +0300

    Game main activity

--- SimpleAndroidGDF/src/com/georgeframework/simpleandroidgdf/GameMainActivity.java
+++ SimpleAndroidGDF/src/com/georgeframework/simpleandroidgdf/GameMainActivity.java
@@ -1,14 +1,25 @@
 package com.georgeframework.simpleandroidgdf;
 
 import android.app.Activity;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 
 public class GameMainActivity extends Activity{
 	
+	public static final int GAME_WIDTH = 800;
+	public static final int GAME_HEIGHT = 450;
+	public static GameView sGame;
+	public static AssetManager assets;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		
+		assets=getAssets();
+		sGame= new GameView(this, GAME_WIDTH, GAME_HEIGHT);
+		setContentView(sGame);		
 	}
+	
+	
+	
 
 }
---
+++ SimpleAndroidGDF/src/com/georgeframework/simpleandroidgdf/GameView.java
@@ -0,0 +1,13 @@
+package com.georgeframework.simpleandroidgdf;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class GameView extends SurfaceView{
+
+	public GameView(Context context , int gameWidth,int gameHeight) {
+		super(context);
+		
+	}
+
+}
