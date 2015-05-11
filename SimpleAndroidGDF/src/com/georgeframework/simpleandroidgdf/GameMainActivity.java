bdc2056458b2821737ee50e56482b27b05413d30
gpapatheodorou <gpapatheodorou@globoplc.com>
2015-05-11 16:54:22 +0300

    Android framework initial step

---
+++ SimpleAndroidGDF/src/com/georgeframework/simpleandroidgdf/GameMainActivity.java
@@ -0,0 +1,14 @@
+package com.georgeframework.simpleandroidgdf;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameMainActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		
+	}
+
+}
